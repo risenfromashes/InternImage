ddb308d402d4ca2cd350e44f66835a4b7e2ba443
Md. Ashrafur Rahman Khan <ashrafurkhan37@gmail.com>
2026-01-19 23:59:04 +0600

    Update segmentation_tensorrt_static-1024x1024.py

--- segmentation/deploy/configs/mmseg/segmentation_tensorrt_static-1024x1024.py
+++ segmentation/deploy/configs/mmseg/segmentation_tensorrt_static-1024x1024.py
@@ -2,7 +2,7 @@ _base_ = ['./segmentation_static.py', '../_base_/backends/tensorrt.py']
 
 onnx_config = dict(input_shape=[1024, 1024])
 backend_config = dict(
-    common_config=dict(max_workspace_size=1 << 30),
+    common_config=dict(max_workspace_size=1 << 33),
     model_inputs=[
         dict(
             input_shapes=dict(
